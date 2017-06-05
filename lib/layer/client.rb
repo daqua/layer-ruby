05d90ccee2fe397d81bb408b966a2be3b88c3bed
Daniel Quanz <danielquanz@poltergeist.chat>
2017-06-05 15:58:47 +0200

    Upgrade Layer Version to 2.0

--- lib/layer/client.rb
+++ lib/layer/client.rb
@@ -67,7 +67,7 @@ module Layer
       url = "https://api.layer.com#{url}" unless url.start_with?('https://api.layer.com')
 
       headers = {
-        'Accept' => 'application/vnd.layer+json; version=1.0',
+        'Accept' => 'application/vnd.layer+json; version=2.0',
         'Content-Type' => 'application/json',
         'If-None-Match' => SecureRandom.uuid
       }.merge(headers)
